a91c5f68313e060d797d0a2050da2d4af77c4395
Benjamin Lewis <23inhouse@gmail.com>
2019-05-23 16:53:09 +0200

    Add poopStains to UserData

--- Brown October/Brown October/UserData.swift
+++ Brown October/Brown October/UserData.swift
@@ -12,6 +12,7 @@ struct UserData {
     enum Key: String {
         case computerGamesWon = "computerGamesWon"
         case humanGamesWon = "humanGamesWon"
+        case poopStains = "poopStains"
     }
 
     static let defaults = UserDefaults.standard
@@ -24,6 +25,23 @@ struct UserData {
     static func storeGamesWon(for player: Player, count: Int) {
         let playerKey = key(for: player)
         defaults.set(count, forKey: playerKey)
+    }
+
+    static func retrievePoopStains(for board: inout Board) -> Bool {
+        if let poopStainData = defaults.array(forKey: Key.poopStains.rawValue) {
+            for data in poopStainData as! [[Int]] {
+                board.poopStains[data[0]] = Board.PoopStain(x: data[1], y: data[2], direction: data[3])
+            }
+
+            return true
+        }
+
+        return false
+    }
+
+    static func storePoopStains(for board: Board) {
+        let data = board.poopStains.map({ [$0.key, $0.value.x, $0.value.y, $0.value.direction]})
+        defaults.set(data, forKey: Key.poopStains.rawValue)
     }
 
     static private func key(for player: Player) -> String {
