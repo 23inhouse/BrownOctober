5e3437df3c8b0ca9c26d72ebe4a757cc1e02a033
Benjamin Lewis <23inhouse@gmail.com>
2019-05-26 12:11:11 +0200

    Add the Game play UI test

---
+++ Brown October/Brown OctoberUITests/GamePlayUITests.swift
@@ -0,0 +1,26 @@
+//
+//  GamePlayUITests.swift
+//  Brown OctoberUITests
+//
+//  Created by Benjamin Lewis on 26/5/19.
+//  Copyright © 2019 Benjamin Lewis. All rights reserved.
+//
+
+import XCTest
+
+class GamePlayUITests: XCTestCase {
+
+    let app = XCUIApplication()
+
+    override func setUp() {
+        continueAfterFailure = false
+
+        app.launchArguments = ["-reset", "-game-state", "play"]
+        app.launch()
+    }
+
+    func testGameSetup() {
+        app.tap()
+        saveScreenshot("game-play.png")
+    }
+}
